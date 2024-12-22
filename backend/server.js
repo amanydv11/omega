8ef004ec9ea2aaedf2a83a76863cd7060accf783
aman <amanraj1172004@gmail.com>
2024-12-22 19:54:58 +0530

    yes

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,8 @@ app.use('/api/auth',authRoutes)
 app.use('/api/user',userRoutes)
 app.use('/api/message',messageRoutes)
 app.use(express.static(path.join(__dirname,'/frontend/dist')));
-app.get('*',(res,req)=>{
-app.sendFile(path.join(__dirname, 'frontend','dist','index.html'))
+app.get('*',(req,res)=>{
+res.sendFile(path.join(__dirname, 'frontend','dist','index.html'))
 })
 
 const PORT = process.env.PORT || 8000
